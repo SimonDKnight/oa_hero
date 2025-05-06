00ea4e1e1bff862f944dd6e57ad05d05d7051358
Simon Knight <simondknight1986@gmail.com>
2025-05-06 07:32:18 +0400

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.js_compressor = nil # if using esbuild
+  config.assets.compile = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
