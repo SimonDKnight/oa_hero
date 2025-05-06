86a120df39022b8015581d0339123aac459623f4
Simon Knight <simondknight1986@gmail.com>
2025-05-06 07:23:44 +0400

    Initial app

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Entry point for the build script in your package.json
+import "./vendor.min.js";
+import "./gr-theme-mode-switcher.js"
+import "./custom.js";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import "../stylesheets/custom.css"
+AOS.init();
