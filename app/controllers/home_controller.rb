8c7be687f7ce3605cb0dcd990c42857f2fc5979c
Simon Knight <simonknight@Mac-Studio-2.local>
2025-06-21 11:02:08 +0100

    Format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+      format.any { render html: "", status: :ok }
+    end
   end
 
   def retailer_list
